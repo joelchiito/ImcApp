d1ea9d88834a8c3d1823bde6dc9bbd8e6236c1ed
Mena May Joel Enrique <16470287@itcampeche.edu.mx>
2019-09-21 15:52:16 -0500

    Proyecto ImcApp, que calcula el Índice de Masa Corporal (IMC).

--- app/src/main/java/android/joelmena/imcapp/ImcFragment.java
+++ app/src/main/java/android/joelmena/imcapp/ImcFragment.java
@@ -4,16 +4,74 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class ImcFragment extends Fragment {
+    private EditText mCampoPeso;
+    private EditText mCampoEstatura;
+    private Button mBotonCalcular;
+    private Button mBotonLimpiar;
+    private TextView mEtiquetaImc;
+    private TextView mSituacionNutricional;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_imc, container, false);
+
+            mCampoPeso = v.findViewById(R.id.campo_peso);
+            mCampoEstatura = v.findViewById(R.id.campo_estatura);
+            mBotonCalcular = v.findViewById(R.id.boton_calcular);
+            mBotonLimpiar = v.findViewById(R.id.boton_limpiar);
+            mEtiquetaImc = v.findViewById(R.id.etiqueta_imc);
+            mSituacionNutricional = v.findViewById(R.id.etiqueta_situacion);
+
+            mBotonCalcular.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String s = mCampoPeso.getText().toString();
+                    double peso = Double.parseDouble(s);
+                    s = mCampoEstatura.getText().toString();
+                    double estatura = Double.parseDouble(s);
+                    double imc = peso / (estatura*estatura);
+                    s = String.format("%2.2f, imc");
+                    mEtiquetaImc.setText(s);
+
+                    if (imc < 18.5){
+                        mSituacionNutricional.setText(R.string.texto_situacion_peso_bajo);
+                    }
+                    else if (imc < 25.0){
+                        mSituacionNutricional.setText(R.string.texto_situacion_peso_normal);
+                    }
+                    else if (imc < 30.0){
+                        mSituacionNutricional.setText(R.string.texto_situacion_sobrepeso);
+                    }
+                    else if (imc < 40.0){
+                        mSituacionNutricional.setText(R.string.texto_situacion_obesidad);
+                    }
+                    else{
+                        mSituacionNutricional.setText(R.string.texto_situacion_obesidad_extrema);
+                    }
+                }
+            });
+
+            mBotonLimpiar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mCampoPeso.setText(" ");
+                    mCampoEstatura.setText(" ");
+                    mEtiquetaImc.setText("0.0");
+                    mSituacionNutricional.setText(" ");
+                }
+            });
 
         return v;
     }
