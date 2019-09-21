9c0f09cd3823727ed0d76b5b311084b7b3d6a588
Mena May Joel Enrique <16470287@itcampeche.edu.mx>
2019-09-21 14:47:49 -0500

    Versión inicial del proyecto ImcApp. No incluye manejo de eventos.

---
+++ app/src/main/java/android/joelmena/imcapp/ImcFragment.java
@@ -0,0 +1,25 @@
+package android.joelmena.imcapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ImcFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_imc, container, false);
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
